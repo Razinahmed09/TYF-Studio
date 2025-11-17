7badd651df74062bde64b391cd451f9cdc5d8b8f
razinahmed09 <razyn.09@gmail.com>
2025-11-17 23:09:31 +0500

    some changes

--- src/app/(homes)/home-1/page.tsx
+++ src/app/(homes)/home-1/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Metadata } from "next";
-import HomeMain from "@/pages/homes/home-1";
+import HomeMain from "@/pages/homes/home-4";
 
 export const metadata: Metadata = {
-  title: "Liko - Home Main",
+  title: "TYF Studio",
 };
 
 
